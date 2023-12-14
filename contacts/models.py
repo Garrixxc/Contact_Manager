3c1588fcfd21dc8a58a55eea5b4b56d3e16010ea
Gaurav Salvi <105053156+Garrixxc@users.noreply.github.com>
2023-12-13 18:08:34 -0800

    dateTime conflict resolved

--- contacts/models.py
+++ contacts/models.py
@@ -1,9 +1,8 @@
 from django.db import models
-from datetime import datetime
-
+import datetime
 class Member(models.Model):
     firstname=models.CharField(max_length=100)
     num=models.CharField(max_length=100)
     email=models.CharField(max_length=100)
     notes=models.CharField(max_length=300)
-    createdTime = models.DateTimeField(default=datetime.now, null=True)
+    createdTime = models.DateTimeField(default=datetime.datetime.now, null=True)
